2f08b60f644e265f185f6e2f291f4a2d97a4c0e6
farhid1 <espichanfarhid@gmail.com>
2025-11-06 03:38:13 -0500

    Antes del caos 2

--- src/app/pages/empleado/clientes/clientes.page.ts
+++ src/app/pages/empleado/clientes/clientes.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 import { FirebaseService } from '../../../services/firebase.service';
 
 @Component({
@@ -9,7 +10,7 @@ import { FirebaseService } from '../../../services/firebase.service';
   templateUrl: './clientes.page.html',
   styleUrls: ['./clientes.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
 })
 export class ClientesPage implements OnInit {
   clientes: any[] = [];
