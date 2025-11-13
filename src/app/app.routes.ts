9c3cffc93e0549d464c6b7ff523b24ba5d1fb2ff
jesus-zr <jesus.a.zr2310@gmail.com>
2025-11-13 14:52:58 -0500

    Comentarios Borrados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,4 @@ export const routes: Routes = [
   { path: 'add-cliente', loadComponent: () => import('./pages/empleado/add-cliente/add-cliente.page').then(m => m.AddClientePage) },
   { path: 'clientes', loadComponent: () => import('./pages/empleado/clientes/clientes.page').then(m => m.ClientesPage) },
   { path: 'editar-cliente/:id', loadComponent: () => import('./pages/empleado/editar-cliente/editar-cliente.page').then(m => m.EditarClientePage) },
-  // Nota: la página 'proyecto' no existe actualmente; cuando se cree, descomentar la ruta:
-  // { path: 'proyecto', loadComponent: () => import('./pages/empleado/proyecto/proyecto.page').then(m => m.ProyectoPage) },
 ];
